ad5976ef85214d1df5fb9b20bcb621f4a20fad6a
WAUT6 <yuriyaacoub@hotmail.com>
2024-06-16 17:51:56 +0300

    auth

---
+++ utils/database.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+let isConnected = false; // Track connection status
+
+export const connectToDB = async () => {
+  mongoose.set("strictQuery", true);
+
+  if (isConnected) {
+    console.log("Using existing connection");
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "share_prompt",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    isConnected = true;
+  } catch (error) {
+    console.error("Error connecting to database", error);
+  }
+};
